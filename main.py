9d7ee8fcb2ed6349fa36773d157be796c264a458
fahadamunir <fahadamunir@gmail.com>
2020-08-27 16:37:06 +0500

    added wiki info and fixed exception bug

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ import random
 import os
 import datetime, calendar
 
-import wikipedia as wiki
+import wikipedia
 import speech_recognition as sr
 from gtts import gTTS
-from va_utils import recordAudio, vaSpeechResponse
+from va_utils import recordAudio, vaSpeechResponse, vaWakeUpCall, getInfoFromWikipedia
 from playsound import playsound
 
-recordAudio()
-test_text = "This is spaaarta!"
-vaSpeechResponse(test_text)
+speech_text = None
+
+while True:
+    speech_text = recordAudio()
+    #print(speech_text)
+
+    #wake_call = vaWakeUpCall(speech_text)
+    #if wake_call is True:
+    #    print("I am awake")
+
+    if speech_text is not None:
+        wiki_info = getInfoFromWikipedia(speech_text)
+        #test_text = "This is spaaarta!"
+        vaSpeechResponse(wiki_info)
--- va_utils.py
+++ va_utils.py
@@ -2,7 +2,7 @@ import random
 import os
 import datetime, calendar
 
-import wikipedia as wiki
+import wikipedia
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
@@ -11,8 +11,8 @@ from playsound import playsound
 # This method wakes up the virtual assitant
 def vaWakeUpCall(text_data):
     text = text_data.lower()
-    wake_phrase = ["hello system"]
-    if wake_phrase in text:
+    wake_phrase = "hello system"
+    if wake_phrase == text:
         return True
     else:
         return False
@@ -27,24 +27,27 @@ def getDateTime():
 
     return None
 
-
 # This method opens the microphone and records the audio
 def recordAudio():
     # defining the recognizer object
     record = sr.Recognizer()
 
     with sr.Microphone() as speaker:
+        record.adjust_for_ambient_noise(speaker)
         print("Assistant: Please talk to me!")
         audio = record.listen(speaker)
 
     audio_data = ""
 
     try:
-        audio_data = record.recognize_google(audio)
+        audio_data = record.recognize_google(audio, language = 'en-US')
         print("You:" , audio_data)
-    except sr.UnknownValueError():
+    except Exception as E:
+        audio_data = None
+        #print(audio_data)
         print("unable to understand the speaker: Unknown Error!")
     except sr.RequestError() as e:
+        audio_data = None
         print(e)
 
     return audio_data
@@ -55,3 +58,8 @@ def vaSpeechResponse(text_data):
     speech.save('va_response/assistant_speech_response.mp3')
     #os.system('start va_response/assistant_speech_response.mp3')
     playsound('va_response/assistant_speech_response.mp3')
+
+# This method gets the information from wikipedia
+def getInfoFromWikipedia(object_info):
+    wiki_text = wikipedia.summary(object_info, sentences = 3)
+    return wiki_text
