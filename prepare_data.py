8068d1443c9c70be2c563853eb4aa063fae9c209
fahadamunir <fahadamunir@gmail.com>
2020-09-01 17:41:15 +0500

    read and parse comments

--- prepare_data.py
+++ prepare_data.py
@@ -2,7 +2,7 @@ import json
 import sqlite3
 import os
 
-
+comments_file = "RC_2015-01"
 comments_time = "2015-01"
 sql_bulk = []
 conn = None
@@ -20,9 +20,43 @@ def create_table():
                     (parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT,
                     comment TEXT, subreddit TEXT, unix INT, score INT) """)
 
+def format_data(comment):
+    comment = comment.replace("\n", " newlinechar ").replace("\r", " newlinechar ").replace('"',"'")
+    return comment
+
+def find_parent(pid):
+    try:
+        sql = "SELECT comment FROM parent_reply WHERE commet_id = '{}' LIMIT 1".format(pid)
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        print ("find parent", e)
+        return False
+
 
 if __name__ == "__main__":
     create_table()
 
 
     # Cleaning the data
+    row_counter = 0
+    paired_counter = 0
+    with open (os.path.join(path, comments_file), buffering = 1000) as comments:
+        for row in comments:
+            row_counter +=1
+            comment = json.loads(row)
+            parent_id = comment['parent_id']
+            body = format_data(comment['body'])
+            created_utc = comment['created_utc']
+            score = comment['score']
+            subreddit = comment['subreddit']
+            parent_data = find_parent(parent_id)
+
+            print (comment)
+            break
+
+        print(os.path.join(path,comments_file))
